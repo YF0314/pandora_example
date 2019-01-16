ce3147270df559ceb83baff937c004226600660b
Yukkyman <46261728+YF0314@users.noreply.github.com>
2019-01-16 11:33:14 +0900

    一方向ハッシュの導入
    よりセキュアになりました。

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 
-#このコードは一部syncer.jpからのコードを引用しています。
+#このコードは一部syncer.jpからのコードを一部、引用しています。
 
 
-$request_url = 'https://api.pandora.xere.jp/oauth/app/{TOKEN}/{USERNAME}' ;
+$request_url = 'https://api.pandora.xere.jp/oauth/app/{USERNAME}/{TOKEN}/' ;
 
+$hmac = hash_hmac('sha256', $request_url, '{SECRET_TOKEN}');
 
 $context = array(
 	'http' => array(
@@ -12,7 +13,7 @@ $context = array(
 	) ,
 ) ;
 
-
+$request_url.=$hmac;
 
 $curl = curl_init() ;
 curl_setopt( $curl, CURLOPT_URL, $request_url ) ;	// リクエストURL
@@ -26,7 +27,7 @@ $res1 = curl_exec( $curl ) ;
 $res2 = curl_getinfo( $curl ) ;
 curl_close( $curl ) ;
 
-
+//以下からは条件分岐をする事をお勧めします。でないとPandora側からエラーが返ってきてあなたが作るアプリケーションもエラーが起きる場合があります。
 // 取得したデータ
 $json = substr( $res1, $res2['header_size'] ) ;	// 取得したデータ(JSONなど)
 
